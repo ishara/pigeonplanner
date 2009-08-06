5274532028a20aa6e0257f878c53601da163a53f
Timo Vanwynsberghe <timovwb@gmail.com>
2009-08-06 22:45:43 +0200

    Fixed little backup problem

--- PigeonPlanner/Widgets.py
+++ PigeonPlanner/Widgets.py
@@ -336,18 +336,18 @@ class BackupDialog(gtk.Dialog):
         if folder:
             success = Backup.make_backup(folder)
             if success:
-                Widgets.message_dialog('info', Const.MSG_BACKUP_SUCCES, self.par)
+                message_dialog('info', Const.MSG_BACKUP_SUCCES, self.par)
             else:
-                Widgets.message_dialog('info', Conts.MSG_BACKUP_FAILED, self.par)
+                message_dialog('info', Conts.MSG_BACKUP_FAILED, self.par)
 
     def restorebackup_clicked(self, widget):
         zipfile = self.fcButtonRestore.get_filename()
         if zipfile:
             success = Backup.restore_backup(zipfile)
             if success:
-                Widgets.message_dialog('info', Const.MSG_RESTORE_SUCCES, self.par)
+                message_dialog('info', Const.MSG_RESTORE_SUCCES, self.par)
             else:
-                Widgets.message_dialog('info', Conts.MSG_RESTORE_FAILED, self.par)
+                message_dialog('info', Conts.MSG_RESTORE_FAILED, self.par)
 
 class ImageWindow(gtk.Window):
     def __init__(self, imagepath, main):
