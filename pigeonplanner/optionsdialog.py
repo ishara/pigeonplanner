7d5f78f759661659a1f51e847504b0d03d077c79
Timo Vanwynsberghe <timovwb@gmail.com>
2010-02-26 16:27:08 +0100

    Added French language to options

--- pigeonplanner/optionsdialog.py
+++ pigeonplanner/optionsdialog.py
@@ -68,6 +68,7 @@ class OptionsDialog:
                 ('Bosnian', 'bs'),
                 ('Dutch', 'nl'),
                 ('English', 'en'),
+                ('French', 'fr'),
                 ('Russian', 'ru'),
                 ('Spanish', 'es')
                 ]
