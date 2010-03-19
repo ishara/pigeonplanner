ed80ecd3b2e3c173eca17e882f020364d28e0320
Timo Vanwynsberghe <timovwb@gmail.com>
2010-03-19 16:49:36 +0100

    Pushed up to version 0.8.2

--- pigeonplanner/__init__.py
+++ pigeonplanner/__init__.py
@@ -17,9 +17,9 @@
 
 name = "Pigeon Planner"
 
-versionbase = "0.8.1"
+versionbase = "0.8.2"
 
-version = "0.8.1.bzr457"
+version = "0.8.2"
 
 copyright = "(C)opyright 2009-2010 Timo Vanwynsberghe"
 
