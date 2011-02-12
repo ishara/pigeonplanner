4bd16385202bde16f0b252e95989d24f56ca6599
Timo Vanwynsberghe <timovwb@gmail.com>
2011-02-12 14:49:21 +0100

    Updated translation template and Portuguese translation

--- pigeonplanner/__init__.py
+++ pigeonplanner/__init__.py
@@ -21,7 +21,7 @@ name = "Pigeon Planner"
 
 versionbase = "0.8.9"
 
-version = "0.8.9.bzr660"
+version = "0.8.9.bzr661"
 
 copyright = "(C)opyright 2009-%s Timo Vanwynsberghe" %datetime.now().year
 
