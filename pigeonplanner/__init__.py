41fcc13d43bef2e152fe942fbbcad9a43f987084
Timo Vanwynsberghe <timovwb@gmail.com>
2011-02-12 20:50:24 +0100

    reduced padding around buttons in results and medication views

--- pigeonplanner/__init__.py
+++ pigeonplanner/__init__.py
@@ -21,7 +21,7 @@ name = "Pigeon Planner"
 
 versionbase = "0.8.9"
 
-version = "0.8.9.bzr664"
+version = "0.8.9.bzr665"
 
 copyright = "(C)opyright 2009-%s Timo Vanwynsberghe" %datetime.now().year
 
