dbaac8bc5d434f15dc0542de283bff422e7c1533
Timo Vanwynsberghe <timovwb@gmail.com>
2014-01-06 18:21:40 +0100

    Pushed up dev version to 2.1.0

--- pigeonplanner/core/const.py
+++ pigeonplanner/core/const.py
@@ -28,7 +28,7 @@ from datetime import datetime
 # No application imports!
 
 
-VERSION_TUPLE = (2, 0, 2)
+VERSION_TUPLE = (2, 1, 0)
 VERSION = ".".join(map(str, VERSION_TUPLE))
 
 NAME = "Pigeon Planner"
