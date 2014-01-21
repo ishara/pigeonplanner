58fb64eb5a5e71e3cbae6969315d7fcfa63f5b49
Timo Vanwynsberghe <timovwb@gmail.com>
2014-01-21 23:59:02 +0100

    Added type, wind and weather fields to the resultparser

--- pigeonplanner/ui/resultparser.py
+++ pigeonplanner/ui/resultparser.py
@@ -124,13 +124,16 @@ class ResultParser(builder.GtkBuilder):
             return
         sector = self.widgets.sectorentry.get_text()
         category = self.widgets.categoryentry.get_text()
+        ftype = self.widgets.typeentry.get_text()
+        wind = self.widgets.windentry.get_text()
+        weather = self.widgets.weatherentry.get_text()
         
         for row in self.widgets.liststore:
             toggle, pindex, ring, year, place = row
             if not toggle: continue
             data = {"pindex": pindex, "date": date, "point": point, "place": place,
-                    "out": out, "sector": sector, "type": "", "category": category,
-                    "wind": "", "weather": "", "comment": "",
+                    "out": out, "sector": sector, "type": ftype, "category": category,
+                    "wind": wind, "weather": weather, "comment": "",
                     "speed": 0.0, "windspeed": ""}
             if database.result_exists(data):
                 logger.info("Pigeon %s already has the selected result" % pindex)
