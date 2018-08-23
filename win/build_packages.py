e4ba3799173e55d9b38c29645f32da4c04e02d4c
Timo Vanwynsberghe <timovwb@gmail.com>
2018-08-23 12:27:11 +0200

    Cleaned up and improved the build script for Windows

--- win/build_packages.py
+++ win/build_packages.py
@@ -10,63 +10,88 @@ sys.path.insert(0, os.path.abspath(".."))
 from pigeonplanner.core.const import VERSION
 
 
-package_root = '..\\dist\\'
+package_root = "..\\dist"
 if not os.path.exists(package_root):
     print("Dist directory not found. Did you py2exe'd?")
     sys.exit()
 
-python_root = os.path.dirname(sys.executable) + "\\"
-gtk_root = python_root + "Lib\\site-packages\\gtk-2.0\\runtime\\"
+python_root = os.path.dirname(sys.executable)
+gtk_root = os.path.join(python_root, "Lib", "site-packages", "gtk-2.0", "runtime")
 
-# Copy the required theme files
-theme_files = [("etc\\gtk-2.0\\", "gtkrc"),
-               ("lib\\gtk-2.0\\2.10.0\\engines\\", "libclearlooks.dll"),
-        ]
-for theme_path, theme_file in theme_files:
-    dist_theme_path = package_root+theme_path
-    if not os.path.exists(dist_theme_path):
-        os.makedirs(dist_theme_path)
-    shutil.copyfile(gtk_root+theme_path+theme_file, dist_theme_path+theme_file)
 
-# Only copy GTK+ translations for the languages used inside Pigeon Planner
-#TODO: only copy gtk20.mo?
-langs = os.listdir(package_root+"languages")
-for lang in langs:
-    try:
-        shutil.copytree(gtk_root+"share\\locale\\"+lang,
-                        package_root+"share\\locale\\"+lang)
-    except WindowsError:
-        pass
+def copy_files():
+    # Copy the required theme files
+    theme_files = [
+        ("etc\\gtk-2.0", "gtkrc"),
+        ("lib\\gtk-2.0\\2.10.0\\engines", "libclearlooks.dll"),
+    ]
+    for theme_path, theme_file in theme_files:
+        dist_theme_path = os.path.join(package_root, theme_path)
+        if not os.path.exists(dist_theme_path):
+            os.makedirs(dist_theme_path)
+        shutil.copyfile(
+            os.path.join(gtk_root, theme_path, theme_file),
+            os.path.join(dist_theme_path, theme_file)
+        )
 
-# Copy intl.dll from GTK+ to avoid problems
-shutil.copy(gtk_root+"bin\\intl.dll", package_root+"lib")
+    # Only copy GTK+ translations for the languages used inside Pigeon Planner
+    # TODO: only copy gtk20.mo?
+    langs = os.listdir(os.path.join(package_root, "languages"))
+    for lang in langs:
+        try:
+            shutil.copytree(
+                os.path.join(gtk_root, "share", "locale", lang),
+                os.path.join(package_root, "share", "locale", lang)
+            )
+        except WindowsError:
+            pass
 
-# Find the mvcr90.dll and approriate manifest
-#TODO: This only works on Windows XP probably
-dll_version = "9.0.21022.8"
-dlls_dir = "C:\\WINDOWS\\WinSxS\\"
-manifests_dir = dlls_dir + "Manifests\\"
+    # Copy intl.dll from GTK+ to avoid problems
+    shutil.copy(
+        os.path.join(gtk_root, "bin", "intl.dll"),
+        os.path.join(package_root, "lib")
+    )
 
-for manifest in glob.glob(manifests_dir + "*.manifest"):
-    if "Microsoft.VC90.CRT" in manifest and dll_version in manifest:
-        shutil.copy(manifest, package_root + "Microsoft.VC90.CRT.manifest")
-        break
-for dll_dir in os.listdir(dlls_dir):
-    if "Microsoft.VC90.CRT" in dll_dir and dll_version in dll_dir:
-        dll = dlls_dir + dll_dir + "\\msvcr90.dll"
-        shutil.copy(dll, package_root)
-        break
+    # Find the mvcr90.dll and approriate manifest
+    # TODO: This only works on Windows XP probably
+    dll_version = "9.0.21022.8"
+    dlls_dir = "C:\\WINDOWS\\WinSxS"
+    manifests_dir = os.path.join(dlls_dir, "Manifests")
 
-# Call the InnoSetup compiler to create the setup file
-iss_compiler = "C:\\Program Files (x86)\\Inno Setup 5\\ISCC.exe"
-subprocess.call([iss_compiler, "setup.iss"])
+    for manifest in glob.glob(manifests_dir + "*.manifest"):
+        if "Microsoft.VC90.CRT" in manifest and dll_version in manifest:
+            shutil.copy(manifest, os.path.join(package_root, "Microsoft.VC90.CRT.manifest"))
+            break
+    for dll_dir in os.listdir(dlls_dir):
+        if "Microsoft.VC90.CRT" in dll_dir and dll_version in dll_dir:
+            dll = os.path.join(dlls_dir, dll_dir, "msvcr90.dll")
+            shutil.copy(dll, package_root)
+            break
 
-# Create a portable zip package
-ziproot = "Pigeon Planner %s" % VERSION
-zfile = zipfile.ZipFile('pigeonplanner-%s-win32.zip' % VERSION, 'w', zipfile.ZIP_DEFLATED)
-for root, dirs, files in os.walk(package_root):
-    for file in files:
-        filepath = os.path.join(root, file)
-        zfile.write(filepath, filepath.replace("..\\dist", ziproot))
-zfile.close()
 
+def build_installer():
+    iss_compiler = "C:\\Program Files (x86)\\Inno Setup 5\\ISCC.exe"
+    subprocess.call([iss_compiler, "setup.iss"])
+
+
+def build_zip():
+    ziproot = "Pigeon Planner %s" % VERSION
+    zfile = zipfile.ZipFile("pigeonplanner-%s-win32.zip" % VERSION, "w", zipfile.ZIP_DEFLATED)
+    for root, dirs, files in os.walk(package_root):
+        for file in files:
+            filepath = os.path.join(root, file)
+            zfile.write(filepath, filepath.replace("..\\dist", ziproot))
+    zfile.close()
+
+
+if __name__ == "__main__":
+    if "-c" in sys.argv or "--copy" in sys.argv:
+        copy_files()
+    elif "-z" in sys.argv or "--zip" in sys.argv:
+        copy_files()
+        build_zip()
+    else:
+        copy_files()
+        build_zip()
+        build_installer()
+    print "Done!"
