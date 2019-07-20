2c48472349b19fd0af20973a8435ea3017340949
Timo Vanwynsberghe <timovwb@gmail.com>
2019-07-20 18:22:33 +0200

    Changed date entry calendar popup to a proper popover

--- pigeonplanner/ui/widgets/date.py
+++ pigeonplanner/ui/widgets/date.py
@@ -51,7 +51,10 @@ class DateEntry(Gtk.Viewport):
 
     def on_icon_pressed(self, widget, icon_pos, event):
         if icon_pos == Gtk.EntryIconPosition.SECONDARY:
-            CalendarPopup(self)
+            popover = CalendarPopover(self)
+            popover.set_relative_to(self)
+            popover.show_all()
+            popover.popup()
 
     def get_editable(self):
         return self._editable
@@ -113,37 +116,29 @@ class DateEntry(Gtk.Viewport):
         self._unwarn()
 
 
-class CalendarPopup(Gtk.Window):
-
-    __gtype_name__ = "CalendarPopup"
+class CalendarPopover(Gtk.Popover):
+    __gtype_name__ = "CalendarPopover"
 
     def __init__(self, entry):
-        Gtk.Window.__init__(self, Gtk.WindowType.POPUP)
-        self.set_type_hint(Gdk.WindowTypeHint.DIALOG)
-        self.set_modal(True)
-        self.set_resizable(False)
-        self.set_destroy_with_parent(True)
-        toplevel = entry.get_toplevel()
-        self.set_transient_for(toplevel)
+        Gtk.Popover.__init__(self)
+        self.set_position(Gtk.PositionType.BOTTOM)
 
-        buttonapply = Gtk.Button()
-        img = Gtk.Image.new_from_stock(Gtk.STOCK_APPLY, Gtk.IconSize.BUTTON)
-        buttonapply.set_image(img)
+        buttonapply = Gtk.Button.new_from_icon_name(Gtk.STOCK_APPLY, Gtk.IconSize.BUTTON)
         buttonapply.connect("clicked", self.on_buttonapply_clicked)
-        buttoncancel = Gtk.Button()
-        img = Gtk.Image.new_from_stock(Gtk.STOCK_CANCEL, Gtk.IconSize.BUTTON)
-        buttoncancel.set_image(img)
+        buttoncancel = Gtk.Button.new_from_icon_name(Gtk.STOCK_CANCEL, Gtk.IconSize.BUTTON)
         buttoncancel.connect("clicked", self.on_buttoncancel_clicked)
-        vbox = Gtk.VBox(False, 8)
-        vbox.pack_start(buttonapply, False, False, 0)
-        vbox.pack_start(buttoncancel, False, False, 0)
+        button_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        button_box.set_halign(Gtk.Align.CENTER)
+        button_box.get_style_context().add_class("linked")
+        button_box.pack_start(buttonapply, False, False, 0)
+        button_box.pack_start(buttoncancel, False, False, 0)
 
         calendar = Gtk.Calendar()
         calendar.connect("day-selected", self.on_day_selected)
         calendar.connect("day-selected-double-click", self.on_day_clicked)
-        hbox = Gtk.HBox()
-        hbox.pack_start(vbox, False, False, 0)
-        hbox.pack_start(calendar, False, False, 0)
+        main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=2)
+        main_box.pack_start(button_box, False, False, 0)
+        main_box.pack_start(calendar, False, False, 0)
 
         self._calendar = calendar
         self._entry = entry
@@ -151,7 +146,7 @@ class CalendarPopup(Gtk.Window):
         if self._saved_date:
             try:
                 year, month, day = map(int, self._saved_date.split("-"))
-                calendar.select_month(month-1, year)
+                calendar.select_month(month - 1, year)
                 calendar.select_day(day)
                 # Check for a valid date, mostly incorrect year
                 datetime.date(year, month, day).strftime(const.DATE_FORMAT)
@@ -161,15 +156,10 @@ class CalendarPopup(Gtk.Window):
                 self._saved_date = date
                 self._entry.set_text(date)
 
-        vp = Gtk.Viewport()
-        vp.add(hbox)
-        self.add(vp)
-        self._set_position()
-        self.show_all()
+        self.add(main_box)
 
-    # Callbacks
     def on_buttonapply_clicked(self, widget):
-        self.destroy()
+        self.popdown()
 
     def on_buttoncancel_clicked(self, widget):
         if not self._entry.can_empty and self._saved_date == "":
@@ -177,11 +167,11 @@ class CalendarPopup(Gtk.Window):
             pass
         else:
             self._entry.set_text(self._saved_date)
-        self.destroy()
+        self.popdown()
 
     def on_day_selected(self, widget):
         year, month, day = self._calendar.get_date()
-        the_date = datetime.date(year, month+1, day)
+        the_date = datetime.date(year, month + 1, day)
 
         if the_date:
             try:
@@ -192,27 +182,4 @@ class CalendarPopup(Gtk.Window):
             self._entry.set_text("")
 
     def on_day_clicked(self, widget):
-        self.destroy()
-
-    # Internal methods
-    def _set_position(self):
-        allocation = self._entry.get_allocation()
-        window = self._entry.get_parent_window()
-        origin = Gdk.Window.get_origin(window)
-        x = origin.x
-        y = origin.y
-
-        x += allocation.x
-        y += allocation.y
-        bwidth = allocation.width
-        bheight = allocation.height
-
-        x += bwidth - self._entry.size_request().width
-        y += bheight
-
-        if x < 0:
-            x = 0
-        if y < 0:
-            y = 0
-        
-        self.move(x, y)
+        self.popdown()
