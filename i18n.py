b9b209076b5bf8e7b92838eda643fcde18f057a2
Timo Vanwynsberghe <timovwb@gmail.com>
2020-03-08 20:41:43 +0100

    Clean up/PEP8 fixes for translation script

--- i18n.py
+++ i18n.py
@@ -24,16 +24,16 @@ import subprocess
 from optparse import OptionParser
 
 
-GLADEDIR = os.path.join('pigeonplanner', 'ui', 'glade')
-LANGDIR = os.path.join('pigeonplanner', 'data', 'languages')
-PODIR = os.path.join('pigeonplanner', 'data', 'po')
+GLADEDIR = os.path.join("pigeonplanner", "ui", "glade")
+LANGDIR = os.path.join("pigeonplanner", "data", "languages")
+PODIR = os.path.join("pigeonplanner", "data", "po")
 POTFILE = os.path.join(PODIR, "pigeonplanner.pot")
 POTFILES = {"Python": os.path.join(PODIR, "POTFILES_PY.in"),
             "Glade": os.path.join(PODIR, "POTFILES_GLADE.in")}
-FORMATS = {"Python": [('bin', None),
-                      ('pigeonplanner', ['.py'])],
-           "Glade": [(GLADEDIR, ['.ui']),
-                     ('data', ['.ui'])]}
+FORMATS = {"Python": [("bin", None),
+                      ("pigeonplanner", [".py"])],
+           "Glade": [(GLADEDIR, [".ui"]),
+                     ("data", [".ui"])]}
 GLADE_TEMPLATE = """<?xml version="1.0" encoding="UTF-8"?>
 <interface>
   <object class="GtkDialog" id="dialog1">
@@ -52,23 +52,24 @@ def _get_files(folder, ext=None):
             files.append(os.path.join(dirpath, filename))
     return files
 
+
 def create_potfiles_in():
     """
     Create a file for each format type that contains the file paths that will
     be searched for translatable strings.
     """
 
-    for format, folders in FORMATS.items():
-        potfile = POTFILES[format]
+    for lang, folders in FORMATS.items():
+        potfile = POTFILES[lang]
         print("Creating %s" % potfile)
         to_translate = []
         for folder, ext in folders:
             to_translate.extend(_get_files(folder, ext))
 
-        f = open(potfile, "wb")
-        for line in to_translate:
-            f.write(line + "\n")
-        f.close()
+        with open(potfile, "w") as f:
+            for line in to_translate:
+                f.write(line + "\n")
+
 
 def create_potfile():
     """
@@ -79,11 +80,12 @@ def create_potfile():
     if not os.path.exists(POTFILE):
         # We need to make sure this file exists because we use the
         # join argument (-j) with xgettext.
-        f = open(POTFILE, 'w')
-        f.close()
-    for format, potfile in POTFILES.items():
-        cmd = ['xgettext', '-j', '--language', format, '-f', potfile, '-o', POTFILE]
+        with open(POTFILE, "w"):
+            pass
+    for lang, potfile in POTFILES.items():
+        cmd = ["xgettext", "-j", "--language", lang, "-f", potfile, "-o", POTFILE]
         subprocess.call(cmd)
+
 
 def create_mo():
     """
@@ -93,22 +95,24 @@ def create_mo():
 
     for path, names, filenames in os.walk(PODIR):
         for filename in filenames:
-            if not filename.endswith('.po'): continue
+            if not filename.endswith(".po"):
+                continue
             lang = os.path.splitext(filename)[0]
             src = os.path.join(path, filename)
-            dest_path = os.path.join(LANGDIR, lang, 'LC_MESSAGES')
-            dest = os.path.join(dest_path, 'pigeonplanner.mo')
+            dest_path = os.path.join(LANGDIR, lang, "LC_MESSAGES")
+            dest = os.path.join(dest_path, "pigeonplanner.mo")
             if not os.path.exists(dest_path):
                 os.makedirs(dest_path)
             if not os.path.exists(dest):
-                print('Compiling %s' %src)
+                print("Compiling %s" % src)
                 subprocess.call(["msgfmt", src, "-o", dest])
             else:
                 src_mtime = os.stat(src)[8]
                 dest_mtime = os.stat(dest)[8]
                 if src_mtime > dest_mtime:
-                    print('Compiling %s' %src)
+                    print("Compiling %s" % src)
                     subprocess.call(["msgfmt", src, "-o", dest])
+
 
 def bug_workaround():
     # There's a bug in xgettext which doesn't extract translatable rows added
@@ -132,20 +136,21 @@ def bug_workaround():
     # We're going to extract these fields ourself and create a new Glade file
     # with the same strings, but in a field xgettext does parse.
 
-    import xml.etree.ElementTree as ET
+    # import xml.etree.ElementTree as ET
+    from xml.etree import ElementTree
 
     corrected = []
     for gladefile in glob.glob("glade/*.ui"):
-        tree = ET.parse(gladefile)
+        tree = ElementTree.parse(gladefile)
         root = tree.getroot()
-        for col in root.iter('col'):
+        for col in root.iter("col"):
             if col.attrib["translatable"] == "yes":
-                corrected.append('    <property translatable="yes">%s</property>'
+                corrected.append("""    <property translatable="yes">%s</property>"""
                                  % col.text)
 
-    twfile = open("data/TranslationWorkaround.ui", "w")
-    twfile.write(GLADE_TEMPLATE % '\n'.join(corrected))
-    twfile.close()
+    with open("data/TranslationWorkaround.ui", "w") as twfile:
+        twfile.write(GLADE_TEMPLATE % "\n".join(corrected))
+
 
 def main():
     parser = OptionParser()
@@ -160,7 +165,7 @@ def main():
         # Remove existing pot-file to start with a clean template.
         try:
             os.remove(POTFILE)
-        except:
+        except OSError:
             pass
         bug_workaround()
         create_potfiles_in()
@@ -168,6 +173,6 @@ def main():
     if options.mo:
         create_mo()
 
+
 if __name__ == "__main__":
     main()
-
