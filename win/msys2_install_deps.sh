9904f3cd7d61ee19f9080ce6a03f97949b659bd6
Timo Vanwynsberghe <timovwb@gmail.com>
2020-05-12 17:12:31 +0200

    Add architecture detection to msys2 installation script

--- win/msys2_install_deps.sh
+++ win/msys2_install_deps.sh
@@ -2,15 +2,21 @@
 
 set -e
 
+if [[ "$MSYSTEM" == "MINGW32" ]]; then
+    MSYS2_ARCH="i686"
+else
+    MSYS2_ARCH="x86_64"
+fi
+
 pacman --noconfirm -S --needed \
     upx \
-    mingw-w64-x86_64-gtk3 \
-    mingw-w64-x86_64-pkg-config \
-    mingw-w64-x86_64-cairo \
-    mingw-w64-x86_64-gobject-introspection \
-    mingw-w64-x86_64-python3 \
-    mingw-w64-x86_64-python3-gobject \
-    mingw-w64-x86_64-python3-cairo \
-    mingw-w64-x86_64-python3-pip \
-    mingw-w64-x86_64-python3-setuptools \
-    mingw-w64-x86_64-osm-gps-map
+    mingw-w64-$MSYS2_ARCH-gtk3 \
+    mingw-w64-$MSYS2_ARCH-pkg-config \
+    mingw-w64-$MSYS2_ARCH-cairo \
+    mingw-w64-$MSYS2_ARCH-gobject-introspection \
+    mingw-w64-$MSYS2_ARCH-python3 \
+    mingw-w64-$MSYS2_ARCH-python3-gobject \
+    mingw-w64-$MSYS2_ARCH-python3-cairo \
+    mingw-w64-$MSYS2_ARCH-python3-pip \
+    mingw-w64-$MSYS2_ARCH-python3-setuptools \
+    mingw-w64-$MSYS2_ARCH-osm-gps-map
