700d8ff4700f600defedfc2e864489882e058805
Timo Vanwynsberghe <timovwb@gmail.com>
2020-09-14 15:09:46 +0200

    Pushed up to 4.0.4

--- pigeonplanner/core/const.py
+++ pigeonplanner/core/const.py
@@ -28,7 +28,7 @@ from datetime import datetime
 # No application imports!
 
 
-VERSION_TUPLE = (4, 0, 3)
+VERSION_TUPLE = (4, 0, 4)
 VERSION = ".".join(map(str, VERSION_TUPLE))
 
 NAME = "Pigeon Planner"
