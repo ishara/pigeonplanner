80d9d901adafdd9f46687619468808b0e80cf906
Timo Vanwynsberghe <timovwb@gmail.com>
2021-08-19 17:34:55 +0200

    Fix coefficient test to not rely on the system's locale

--- tests/test_common.py
+++ tests/test_common.py
@@ -16,6 +16,8 @@
 # along with Pigeon Planner.  If not, see <http://www.gnu.org/licenses/>
 
 
+import locale
+
 import nose.tools as nt
 
 from pigeonplanner.core import common
@@ -27,18 +29,37 @@ def test_coefficient():
 
     value = common.calculate_coefficient(1, 370)
     nt.assert_almost_equal(value, 0.2703, 4)
-    value = common.calculate_coefficient(1, 370, True)
-    nt.assert_equal(value, "0.2703")
 
     value = common.calculate_coefficient(3, 981)
     nt.assert_almost_equal(value, 0.3058, 4)
-    value = common.calculate_coefficient(3, 981, True)
-    nt.assert_equal(value, "0.3058")
 
     value = common.calculate_coefficient(30, 370)
     nt.assert_almost_equal(value, 8.1081, 4)
-    value = common.calculate_coefficient(30, 370, True)
-    nt.assert_equal(value, "8.1081")
+
+
+def test_coefficient_string():
+    loc_orig = locale.getlocale(locale.LC_NUMERIC)
+    loc_c = "C"
+    loc_nl = ("nl_BE", "UTF-8")
+
+    locale.setlocale(locale.LC_NUMERIC, loc_c)
+    value = common.calculate_coefficient(1, 370, True)
+    nt.assert_equal(value, "0.2703")
+
+    locale.setlocale(locale.LC_NUMERIC, loc_nl)
+    value = common.calculate_coefficient(1, 370, True)
+    nt.assert_equal(value, "0,2703")
+
+    locale.setlocale(locale.LC_NUMERIC, loc_c)
+    value = common.calculate_coefficient(3, 981, True)
+    nt.assert_equal(value, "0.3058")
+
+    locale.setlocale(locale.LC_NUMERIC, loc_nl)
+    value = common.calculate_coefficient(3, 981, True)
+    nt.assert_equal(value, "0,3058")
+
+    locale.setlocale(locale.LC_NUMERIC, loc_orig)
+
 
 def test_escape_text():
     value = common.escape_text(None)
@@ -51,4 +72,3 @@ def test_escape_text():
     nt.assert_equal(value, "&lt;Edit&gt;")
     value = common.escape_text("Spam & eggs")
     nt.assert_equal(value, "Spam &amp; eggs")
-
